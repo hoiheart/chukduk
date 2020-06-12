7247f853ce595b709b00981c9fc3c56d06b0209d
hoiheart <hoiheart@gmail.com>
2020-06-13 08:10:14 +0900

    refactor(api): 커뮤니티 리스트

--- api/graphql/schema.ts
+++ api/graphql/schema.ts
@@ -74,40 +74,25 @@ interface CommunityArgs {
 
 const resolvers = {
   Query: {
-    getCommunityList: async (parent, { type, title, bbs, category, lastID }: CommunityArgs, context, info) => {
+    getCommunityList: async (parent, { type, title, bbs, category, lastID }: CommunityArgs) => {
       const query = {
         $and : [
-          { title : title ? { $regex: new RegExp(title, "im") } : {$exists: true} },
-          { category : category ? category : {$exists: true} }
+          { _id: lastID ? { $lt: lastID } : { $exists: true } },
+          { title : title ? { $regex: new RegExp(title, "im") } : { $exists: true } },
+          { bbs : bbs ? bbs : { $exists: true } },
+          { category : category ? category : { $exists: true } },
+          { date : type ? { $gt : type === 'daily' ? yesterday : week } : { $exists: true } },
+          { views: type ? { $gt : 0 } : { $exists: true } }
         ]
       }
 
-      console.log(query)
-
       try {
-        const result = await community.find(query)
+        const result = await community.find(query).sort(type ? { views: -1, _id: -1 } : { _id: -1 }).limit( page_size )
         return result
       } catch (e) {
         console.log(e)
         return e
       }
-
-      /*
-      let find = { $and : [ { hidden: { $ne: true } }, { title : { $regex: new RegExp(result.query.query, "i") } }, { views : { $gt: 0 } } ] };
-
-			// case : 카테고리 게시판
-			(req.params.category === 'daily') && find.$and.push({ 'date' : { $gt : yesterday, $lte : time } });
-			(req.params.category === 'weekly') && find.$and.push({ 'date' : { $gt : week, $lte : time } });
-			// case : 더보기
-			(req.query.lastItemID) && find.$and.push({ $or: [ 
-				{ $and : [ { views: { $eq: parseInt(req.query.lastItemViews) } }, { _id: { $lt: mongo.ObjectId(req.query.lastItemID) } } ] },
-				{ views: { $lt: parseInt(req.query.lastItemViews) } }
-			] });
-
-			const total = yield collection.find( find ).sort({ views : -1, _id: -1 }).count();
-			let items = yield collection.find( find ).sort({ views : -1, _id: -1 }).limit( listSize ).toArray();
-      */
-      
     },
     getMediaList: async () => {
       const result = await media.find()
