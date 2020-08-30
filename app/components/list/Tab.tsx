aad299c84e94fca4eb68419c6851d017844c7dff
hoiheart <hoiheart@gmail.com>
2020-08-31 08:13:26 +0900

    feat: :lipstick: tab

--- app/components/list/Tab.tsx
+++ app/components/list/Tab.tsx
@@ -20,17 +20,17 @@ const Tab = ({ menu }: { menu: Menu }) => {
       { key: 'weekly', title: 'Weekly' },
       { key: 'fmkorea', title: 'FM코리아' },
       { key: 'soccerline', title: '싸커라인' },
-      { key: 'bundes', title: '분데스매니아' },
-      { key: 'seriea', title: '세리에매니아' },
+      { key: 'bundes', title: '분데스' },
+      { key: 'seriea', title: '세리에' },
       { key: 'gasengi', title: '가생이' },
       { key: 'ppomppu', title: '뽐뿌' }
     ],
     media: [
       { key: 'all', title: '전체' },
       { key: 'epl', title: 'EPL' },
-      { key: 'primera', title: '프리메라리가' },
-      { key: 'bundesliga', title: '분데스리가' },
-      { key: 'seria', title: '세리에A' },
+      { key: 'primera', title: 'LaLiga' },
+      { key: 'bundesliga', title: 'Bundes' },
+      { key: 'seria', title: 'SerieA' },
       { key: 'uefa', title: 'UEFA' },
       { key: 'epl', title: 'EPL' },
       { key: 'etc', title: '기타' }
@@ -47,7 +47,7 @@ const Tab = ({ menu }: { menu: Menu }) => {
 
   return (
     <div className="tab">
-      <Tabs tabs={categories} renderTabBar={props => <Tabs.DefaultTabBar {...props} renderTab={TabLink} page={3} />} />
+      <Tabs tabs={categories} renderTabBar={props => <Tabs.DefaultTabBar {...props} renderTab={TabLink} page={categories.length} />} />
     </div>
   )
 }
