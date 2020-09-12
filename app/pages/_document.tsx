1ab85e00ab71694b0adbe3967b70aed707834361
hoiheart <hoiheart@gmail.com>
2020-09-13 07:29:12 +0900

    chore(app): :chart_with_upwards_trend: GA

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -9,7 +9,22 @@ class MyDocument extends Document {
   render () {
     return (
       <Html lang="ko">
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-82159025-1"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'UA-82159025-1');
+              `
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
