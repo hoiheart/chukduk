c133302a166b92c02ddbcec63d7fe68a8daa0b42
hoiheart <hoiheart@gmail.com>
2021-02-22 17:43:09 +0900

    chore: update cert

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ git reset --hard HEAD
 git pull
 
 # docker-compose
-echo $REG_TOKEN | docker login ghcr.io -u hoiheart --password-stdin
-#docker image prune -f
-docker-compose pull
-docker-compose up -d
+# echo $REG_TOKEN | docker login ghcr.io -u hoiheart --password-stdin
+# docker image prune -f
+# docker-compose pull
+# docker-compose up -d
